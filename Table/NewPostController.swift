fab8c03cf380be4fef23b916ea161fe61d131c29
Henri El Daher <henrieldaher@gmail.com>
2017-11-10 14:41:11 -0500

    created user object and check if current user is an existing user

--- Table/NewPostController.swift
+++ Table/NewPostController.swift
@@ -17,11 +17,14 @@ class NewPostController: UIViewController {
     var givenName: String?
     var familyName: String?
     var userName: String?
-    //var userRecordID: CKRecordID?
+    var userRecordID: CKRecordID?
+    
+    var user: CKRecord?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getUserName()
+        
     }
     
     //note to self: will need to handle user denying permission, and let know necessay to use app
@@ -44,6 +47,8 @@ class NewPostController: UIViewController {
                                 self.familyName = userInfo?.nameComponents?.familyName
                                 self.userName = (userInfo?.nameComponents?.givenName)! + " " + (userInfo?.nameComponents?.familyName)! as String
                                 
+                                self.userRecordID = userInfo!.userRecordID
+                                
                                 //print("CK User Name: " + self.userName!)
                                 //print("\(self.givenName)")
                                 //print("\(self.familyName)")
@@ -65,53 +70,48 @@ class NewPostController: UIViewController {
             let newPost = CKRecord(recordType: "Post")
             newPost["content"] = postContent.text as NSString
             
-            let newAuthor = CKRecord(recordType: "Author")
-            newAuthor["name"] = self.userName! as NSString
-            //here I think I want to assing newAuthor["uniqueID"] = self.CKUserIdentity
-            //(or whatever I call it when I create an instnce variable and assign value in getUserName
+            //1am 11/11 change
+            let author = user
             
-            /*
-            if let userRecordID = userRecordID as? CKRecordID {
-                print("great")
-            }
-            */
- 
-            //newAuthor[""] = self.userRecordID!
+            //let author = CKRecord(recordType: "User")
+            //author["name"] = self.userName! as NSString
             
-            /*
-            let newAuthor = CKRecord(recordType: "Author")
-            newAuthor["name"] = "Steve" as NSString
-            */
- 
-            /*
-            let newAuthorRecordID = CKRecordID(recordName: "115")
-            let newAuthor = CKRecord(recordType: "Author", recordID: newAuthorRecordID)
-            newAuthor["name"] = "Woz" as NSString
-            */
+            //here maybe instead of creating a new parent i want to fetch the existing user
+            //i.e. maybe pass the current user through a segue?
+            //so want a 'user' variable that is a ckrecord passed to this controller
+            //so need to go to timeline controller to create a prepare for segue that passes user
+            //and need to have a user type ckrecord
+            //let author = user
             
             //let reference = CKReference(record: newAuthor, action: CKReferenceAction.none)
-            let reference = CKReference(recordID: newAuthor.recordID, action: CKReferenceAction.deleteSelf)
             
-            newPost["poster"] = reference
-            
-            //should handle if CKError.notAuthenticated.raw value to tell user to login to icloud
-            let db = CKContainer.default().publicCloudDatabase
-            
-            db.save(newAuthor, completionHandler: { (record, error) in
-                if error != nil {
-                    print(error!.localizedDescription)
-                } else {
-                    print("author saved")
-                }
-            })
-            
-            db.save(newPost, completionHandler: { (record, error) in
-                if error != nil {
-                    print(error!.localizedDescription)
-                } else {
-                    print("post saved")
-                }
-            })
+            if let author = author {
+                let reference = CKReference(recordID: author.recordID, action: CKReferenceAction.deleteSelf)
+                
+                newPost["poster"] = reference
+                
+                //should handle if CKError.notAuthenticated.raw value to tell user to login to icloud
+                let db = CKContainer.default().publicCloudDatabase
+                
+                /*
+                db.save(author, completionHandler: { (record, error) in
+                    if error != nil {
+                        print(error!.localizedDescription)
+                    } else {
+                        print("author saved")
+                    }
+                })
+                */
+                
+                
+                db.save(newPost, completionHandler: { (record, error) in
+                    if error != nil {
+                        print(error!.localizedDescription)
+                    } else {
+                        print("post saved")
+                    }
+                })
+            }
             
         }
         
@@ -119,9 +119,7 @@ class NewPostController: UIViewController {
         //first need to create another object though
         //perhaps create an author object
         //can do this in code
-        
 
-        
         dismiss(animated: true, completion: nil)
     }
     
@@ -130,7 +128,32 @@ class NewPostController: UIViewController {
 
 
 
+//newAuthor["recordID"] = self.userRecordID! as CKReference
 
+//options:
+//1) see if I can add a field to the 'user'
+
+//here I think I want to assing newAuthor["uniqueID"] = self.CKUserIdentity
+//(or whatever I call it when I create an instnce variable and assign value in getUserName
+
+/*
+ if let userRecordID = userRecordID as? CKRecordID {
+ print("great")
+ }
+ */
+
+//newAuthor[""] = self.userRecordID!
+
+/*
+ let newAuthor = CKRecord(recordType: "Author")
+ newAuthor["name"] = "Steve" as NSString
+ */
+
+/*
+ let newAuthorRecordID = CKRecordID(recordName: "115")
+ let newAuthor = CKRecord(recordType: "Author", recordID: newAuthorRecordID)
+ newAuthor["name"] = "Woz" as NSString
+ */
 
 
 
--- Table/TimelineController.swift
+++ Table/TimelineController.swift
@@ -12,9 +12,14 @@ import CloudKit
 class TimelineController: UITableViewController {
     
     var posts = [CKRecord]()
+    var users = [CKRecord]()
     let container = CKContainer.default()
     var userName: String?
+    var userRecordID: CKRecordID?
     var date: Date?
+    
+    var isExistingUser = false
+    var user: CKRecord?
     
     //let db = CKContainer.default().publicCloudDatabase
     
@@ -22,20 +27,82 @@ class TimelineController: UITableViewController {
         super.viewDidLoad()
         getUserName()
         loadPosts()
+        fetchUsers()
     }
     
     func checkIfFirstTimeUser() {
         //check if user has never logged on before
         //how? get ckredord id and loop through existing user base to see
+        //print(userRecordID!)
+        //with above have userRecordID
+        //now need to loop thorugh existing user base
+        //so need to fetch all users (before then looping through
+        //so need to create a fetchUsers function
+
+        //need to check if current user (based on getUserName) exists in users array
+        //issue is that the current user's record id does not match the existing user's record ids?
+        //why? if i have the same info shouldn't it be the same recordid
+        //or i guess a different one is made each time?
+        
+        //<CKRecordID: 0x60000003cf00; recordName=_c286ece40a902a4ad2b3058a56e691d1, zoneID=_defaultZone:__defaultOwner__>
+        //<CKRecordID: 0x60800003ff00; recordName=C25510DD-CDE4-4C76-8687-8388A711EC07, zoneID=_defaultZone:__defaultOwner__>
+        //why is post not showing up?
+
+        for user in users {
+            if user["CKID"]! as! String == userRecordID!.recordName {
+            //if user.recordID.recordName == userRecordID!.recordName {
+                print("Got it")
+                isExistingUser = true
+                self.user = user
+                return
+            } else {
+                print("don't got it")
+            }
+        }
+        
+        if isExistingUser == false {
+            createUser()
+        }
+        
+        //authors[0].recordID
+        
         //see if can make user - need to look up this class
         //if not first time user, then call 'createUser() mthod
     }
     
+    func fetchUsers() {
+        users = [CKRecord]()
+        
+        let query = CKQuery(recordType: "User", predicate: NSPredicate(format: "TRUEPREDICATE", argumentArray: nil))
+        let db = container.publicCloudDatabase
+        db.perform(query, inZoneWith: nil) { (results:[CKRecord]?, error:Error?) in
+            if let users = results {
+                self.users = users
+            }
+        }
+    }
+    
     func createUser() {
-        //newAuthor = ... (use code from other file
+        //newAuthor = ... (use code from other file)
         //then in other file will need to assign a new post to the current user
         //above may be tricky
+        let newUser = CKRecord(recordType: "User")
+        newUser["name"] = self.userName! as NSString
+        newUser["CKID"] = self.userRecordID!.recordName as NSString
+        
+        let db = container.publicCloudDatabase
+        
+        db.save(newUser, completionHandler: { (record, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("user saved")
+                self.user = record
+            }
+        })
     }
+    
+    //<CKRecordID: 0x604000025d20; recordName=_c286ece40a902a4ad2b3058a56e691d1, zoneID=_defaultZone:__defaultOwner__>
     
     //note to self: will need to handle user denying permission, and let know necessay to use app
     func getUserName() {
@@ -47,6 +114,7 @@ class TimelineController: UITableViewController {
                     if error != nil {
                         print("handle error")
                     } else {
+                        self.userRecordID = recordId
                         self.container.discoverUserIdentity(withUserRecordID: recordId!, completionHandler: { (userInfo, error) in
                             if error != nil {
                                 print("handle other error")
@@ -54,6 +122,11 @@ class TimelineController: UITableViewController {
                                 self.userName = (userInfo?.nameComponents?.givenName)! + " " + (userInfo?.nameComponents?.familyName)! as String
                                 //print("CK User Name: " + self.userName!)
                                 //print("\(self.givenName)")
+                                
+                                //self.userRecordID = userInfo!.userRecordID
+                                self.checkIfFirstTimeUser()
+                                //self.createUser()
+                                
                                 DispatchQueue.main.async {
                                     self.tableView.reloadData()
                                 }
@@ -113,6 +186,7 @@ class TimelineController: UITableViewController {
             postLabel.text = postContent
             dateLabel.text = dateString
         
+            //note that postAuthor is a user
             if let postAuthor = post["poster"] as? CKReference {
                 //print ("\(postAuthor.recordID)")
                 let authorID = postAuthor.recordID
@@ -122,7 +196,7 @@ class TimelineController: UITableViewController {
                         print(error!.localizedDescription)
                     } else {
                         //not sure why this code seems to run twice
-                        print("ok!")
+                        //print("ok!")
                         if let record = record {
                             DispatchQueue.main.async {
                                 nameLabel.text = record["name"]! as? String
@@ -141,5 +215,44 @@ class TimelineController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 88.0
     }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //newPost
+        if segue.identifier == "newPost" {
+            let controller = segue.destination as! NewPostController
+            controller.user = user
+        }
+    }
+    
+    
+    /*
+     trying not to create a new author every time
+     i.e., do not want 'user' and author', but just 'user' - these refer to the same thing
+     if I make two posts, I don't want 2 authors created
+     
+     I want an author or user to be created when I first log in,
+     and to be identified when I log in from then on
+     
+     so first time I log in I want to create user
+     newUser = CKRecord(type: "User")
+     newUser["name"] = self.userName! (CKRecordID will automatically be created)
+     
+     BUT, only wnat to do this if new user
+     otherwise, instead of creating a new user, want to identify and fetch new user
+     How?
+     check current user's info against stored existing users
+     check current user's record id against stored existing users' recordIDs
+     BUT
+     how can existing users have stored recordIDs if can't assing them one? BECAUSE they are automatically created and accessible
+    */
 
 }
+
+
+
+
+
+
+
+
